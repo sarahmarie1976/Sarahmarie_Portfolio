be10764a5252c97282492448422f3b24929460f7
sarahmarie1976 <sholle7@gmail.com>
2021-07-11 01:21:48 -0500

    :tada: created testimonial section

--- src/assets/data/testimonials.js
+++ src/assets/data/testimonials.js
@@ -1,27 +1,35 @@
 const testimonials = [
   {
     id: 1,
-    name: 'Shaif Arfan',
-    title: 'CEO',
-    org: 'WEB CIFAR',
+    name: 'Hamza Harmouche',
+    title: 'Full-Stack Devloper',
+    org: 'Lambda -> Student',
     desc:
-      'Ayan is a person of commitment. He is really good at what he is doing. I really like his work. Especially the unique point of view of designing.',
+      'I had the honor to work with Sarahmarie. she is such a brilliant developer. Her collaboration and support to the team were second to none. I admire her self-confidence, creativity and work ethics.',
   },
   {
     id: 2,
-    name: 'Fahim Elahe',
-    title: 'Co-founder',
-    org: 'Kreetive',
+    name: 'Nadia Childs',
+    title: 'Full-Stack Devloper',
+    org: 'Lambda -> Student',
     desc:
-      'Really amazing communication skills. Always understand what I am trying to achieve. Also, his work is really amazing. Do really high-quality work.',
+      'I had the pleasure of working with Sarahmarie on a team of student developers while we were completing Lambda School’s Full Stack program. I was very impressed by her attention to detail, professionalism, and eagerness to learn as much as possible. These traits, combined with her warmth and ability to facilitate a friendly team environment will make her a strong asset to any team.',
   },
   {
     id: 3,
-    name: 'Sayed',
-    title: 'Marketing Head',
-    org: 'Eleven Dawn',
+    name: 'Marlon Del Rosario',
+    title: 'Full-Stack Devloper',
+    org: 'Lambda -> Mentor',
     desc:
-      'Really hard-working person. Deliver the work as promised. Planning to work more with this person.',
+      "I worked with Sarahmarie when I was a team lead for a cohort she was in. The quality that I immediately noticed about her is her leadership. Part of being a great leader is being a great communicator. If she's not certain about something, she'll reach out. As great of an engineer, as she is, she's always open to admit if she's having difficulties with some things which makes working with her a lot easier as you won't have any of those bad kinds of surprises.She also shows this eagerness to improve and expand on what she already knows. Although we're not taught about UX designs, she willingly volunteered to do the designing for her team being mentored by someone from Lambda. With the amount of time they were given to complete this project, I think that shows someone's willingness to improve and help their team out. While others would be scared of doing something you haven't done before, Sarahmarie sees opportunities for growth. And so with that, it's not hard to see how having someone as great as Sarahmarie, as a person and engineer, will help any company that she might end up working for. I know she's ready to contribute to any team immediately and with her potential and eagerness to grow, I honestly believe the sky's the limit for her.",
+  },
+  {
+    id: 4,
+    name: 'Timothy Eakin',
+    title: 'Data Scientist',
+    org: 'Lambda -> Student',
+    desc:
+      'Sarahmarie has a true passion for learning and sharing her knowledge with her peers. Her attention to detail and documentation while learning new skills is second to none. Sarahmarie would be one of the most valuable assets on any tech team that values diligence and conscientiousness.',
   },
 ];
 export default testimonials;
